7475f9f293d181b8e8eafbab1bf39019ec51e522
Omkar Sheral <osheral@thoughtworks.com>
2025-01-22 18:10:44 +0530

    Test: Board class: getCellAtCoordinates(): should return the cell at given coordinates | Omkar

--- src/__test__/Board.test.ts
+++ src/__test__/Board.test.ts
@@ -24,6 +24,25 @@ describe("Board class", () => {
     );
   });
 
+  describe("getCellAtCoordinates()", () => {
+
+    it.each([
+      {row: 0, column: 0, name: "A1"},
+      {row: 4, column: 4, name: "E5"},
+      {row: 7, column: 7, name: "H8"},
+    ])
+    (
+      "should return the cell at given coordinates",
+      ({row, column, name}) => {
+        const board = new Board(new CellNameGenerator());
+
+        const cell = board.getCellAtCoordinates(row, column);
+
+        expect(cell?.getName()).toBe(name);
+      },
+    );
+  });
+
   describe("getCellByName()", () => {
 
     it.each(["A1", "E5", "H8"])("should return the cell with given name", (cellName) => {
